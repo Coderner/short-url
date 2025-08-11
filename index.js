3eb7171c4cc086e353216a694ead8e68a0bf37f5
Coderner <shreyashukla680@gmail.com>
2025-08-11 13:41:46 +0530

    created a server side home page

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const path = require("path");
 const {connectToMongoDb} = require("./connection");
 const urlRoute = require("./routes/url");
 const redirectRoute = require("./routes/redirect");
+const URL = require("./models/url");
+const staticRouter = require("./routes/staticRouter");
 
 const app = express();
 const PORT = 8001;
@@ -9,7 +12,16 @@ const PORT = 8001;
 connectToMongoDb("mongodb://localhost:27017/short-url")
 .then(()=>console.log("database connected successfully!"));
 
+app.set("view engine","ejs");
+app.set("views", path.resolve("./views"));
+
+//middleware to support json data
 app.use(express.json());
+
+//middleware to support form data
+app.use(express.urlencoded({extended: false}));
+
+app.use("/",staticRouter);
 app.use("/url",urlRoute);
 app.use("/:shortId", redirectRoute);
 
---
+++ routes/staticRouter.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const URL = require("../models/url")
+
+const router = express.Router();
+
+router.get("/", async(req,res)=>{
+   const urls = await URL.find({});
+   return res.render("home",{
+        urls: urls
+   });
+});
+
+module.exports = router;
+
